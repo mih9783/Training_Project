88c27b7e077882ce9115d743325d46911deb7fba
ivanchikin <ivan.chikin.72@mail.ru>
2019-05-27 19:31:20 +0700

    14-1 псевдомассивы

--- Lessons_JS/14_Lesson/script.js
+++ Lessons_JS/14_Lesson/script.js
@@ -87,6 +87,8 @@ fruit1.push("вишня", "абрикос");
 
 document.write("В массиве fruit " + fruit1.length + " элементов: <br/>");
 document.write(fruit1); // яблоки,груши,сливы,вишня,абрикос
+/////////////////////////////////////////////////
+
 
 // POP()
 /*
@@ -111,10 +113,7 @@ let strLast = str.pop();
 console.log(str);
 console.log(strLast);
 console.log(str[str.length - 1]);
-
-
-
-
+///////////////////////////////////////////
 
 
 // SHIFT()
@@ -131,7 +130,7 @@ for (var i = 0; i < fruit3.length; i++) {
   document.write(fruit3[i] + "<br/>");
 }
 
-let NuM = [1,2,3,4,5,];
+let NuM = [1, 2, 3, 4, 5,];
 let newNum = NuM.shift();
 console.log(NuM);
 console.log(newNum);
@@ -141,8 +140,9 @@ console.log(newNum);
 Метод unshift() добавляет новый элемент в начало массива:
 */
 var fruit4 = ["яблоки", "груши", "сливы"];
-fruit4.unshift("абрикосы");
+fruit4.unshift("абрикосы", "vbfde");
 console.log(fruit4);
+///////////////////////////////
 
 
 // SPLICE()
@@ -182,18 +182,20 @@ console.log(Users3);         // [ "Tom", "Ann", "Bob", "Kate" ]
  В данном случае удаляем три элемента с 1-й по 3-й индексы и вместо них вставляем два элемента.
  */
 
- let num = [1, 2, 3, 4, 5, 6, 7];
- console.log(num);
+let num = [1, 2, 3, 4, 5, 6, 7];
+console.log(num);
 
- num.splice(3, 2);
- 
- console.log(num);
+num.splice(3, 2);
 
- num.splice(3, 0, "четыре", "пять");
- console.log(num);
+console.log(num);
 
- num.splice(0,2,"один","два");
- console.log(num);
+num.splice(3, 0, "четыре", "пять");
+console.log(num);
+
+num.splice(0, 2, "один", "два");
+console.log(num);
+///////////////////////////////////
+
 
 // CONCAT()
 
@@ -207,6 +209,12 @@ var products = Fruit.concat(vegetables);
 for (var i = 0; i < products.length; i++)
   document.write("<br><br>" + products[i] + "<br/>");
 
+let mynum = [1, 2, 3, 4];
+let mynum1 = [5, 6, 7, 8];
+let mynum2 = [9, 10];
+let total = mynum.concat(mynum1, mynum2);
+console.log(total.reverse());
+
 /*
 При этом необязательно объединять только однотипные массивы. Можно и разнотипные:
 */
@@ -215,11 +223,427 @@ var Fruit1 = ["яблоки", "груши", "сливы"];
 var prices = [20, 30, 80];
 var products1 = Fruit1.concat(prices);
 console.log(products1);
+///////////////////////////////////////
+
 
 // JOIN
 /*
 Метод join() объединяет все элементы массива в одну строку:
 */
+var F = [1, 2, 3, 4, 5];
+//var fr = F.join();
+console.log(F.join("-"));
+
 var Fruit2 = ["яблоки", "груши", "сливы", "абрикосы", "персики"];
-var fruitString = Fruit2.join(", ");
+var fruitString = Fruit2.join();
 console.log(fruitString);
+/////////////////////////////
+
+
+// SPLIT()
+
+/*
+метод split() позволяет разбить строки на массив подстрок, используя заданную 
+строку разделитель для определения места разбиения.
+*/
+
+var string1 = "H e l l o", // инициализируем переменные, содержащие строки
+  string2 = "W,o,r,l,d",
+  string3 = "abbaabbaabba",
+  string4 = "H e l l o w o r l d";
+
+
+string1.split(); // возвращаемое значение ["H e l l o"]
+string1.split(" "); // возвращаемое значение ["H", "e", "l", "l", "o"]
+string2.split(","); // возвращаемое значение ["W", "o", "r", "l", "d"]
+string3.split("bb"); // возвращаемое значение ["a", "aa", "aa", "a"] 
+string4.split(" ", 4); // возвращаемое значение ["H", "e", "l", "l"]
+
+// регулярное выражение в качестве разделителя
+
+var string5 = "a2b3c4d"; // инициализируем строковую переменную
+var numbers = /[0-9]/; // инициализируем переменную, содержащую регулярное выражение
+
+string5.split(numbers); // возвращаемое значение ["a", "b", "c", "d"]
+
+//let ans1 = prompt("1 вопрос"),
+//ans2 = prompt("2 вопрос");
+// let arr1 =[];
+// let arr2 =[];
+
+// arr1 = ans1.split(",");
+// arr2 = ans2.split(" ");
+
+// console.log(arr1);
+// console.log(arr1.length);
+// console.log(arr2);
+// console.log(arr2.length);
+
+// СОРТИРОВКА МАССИВА
+
+let arrStr = ["конь", "тыква", "голова", "арбуз", "ёлка"];
+console.log(arrStr.sort());
+
+let arrNum = [32, 3, 8, 6, 35, 1];
+arrNum.sort(sNum);
+function sNum(a, b) {
+  return a - b;
+}
+console.log(arrNum);
+
+let arrNum1 = [32, 3, 8, 6, 35, 1],
+  iSort1 = arrNum1.sort(sNum);
+function sNum1(a, b) {
+  return b - a;
+}
+console.log(iSort1);
+
+let arrSort = [8, 0, 6, 12, 54, 7];
+function myS(a, b) {
+  if (a > b) {
+    return -1;
+  } else if (a == b) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
+
+console.log(arrSort.sort(myS));
+///////////////////////////////////
+
+
+// Поиск индекса элемента
+/*
+Методы indexOf() и lastIndexOf() возвращают индекс первого и последнего включения элемента в массиве. Например:
+*/
+
+///// indexOf
+/////////////////////
+let fruiT = ["яблоки", "груши", "сливы", "яблоки", "груши"];
+
+let firstIndex = fruiT.indexOf("яблоки");
+let lastIndex = fruiT.lastIndexOf("яблоки");
+let otherIndex = fruiT.indexOf("вишни");
+console.log(firstIndex); // 0
+console.log(lastIndex);  // 3
+console.log(otherIndex); // -1
+
+let f = ["яблоки", "груши", "сливы", "яблоки", "груши"];
+console.log(f.indexOf("яблоки"));
+console.log(f.lastIndexOf("яблоки"));
+/*
+firstIndex имеет значение 0, так как первое включение стоки "яблоки" в массиве приходится на индекс 0, а последнее на индекс 3.
+
+Если же элемент отсутствует в массиве, то в этом случае методы indexOf() и lastIndexOf() возвращают значение -1.
+*/
+// синтаксис array.indexOf( searchElement, fromIndex )
+/*
+searchElement --- Значение, которое требуется найти внутри массива. Обязательное значение.
+
+fromIndex ---  Целое число, определяющее индекс массива, с которого начинается поиск элемента массива. Необязательное значение, если не указано, то поиск осуществляется с 0 индекса. 
+Допускается использование отрицательных значений, в этом случае индекс с которого будет произведено сравнение элементов будет расчитан по следующей формуле: length (длина массива) + fromIndex.
+*/
+
+// пример использования
+
+var x = [1, 2, 3, "a", "b", "c"]; // инициализируем переменную, содержащую массив
+
+console.log(x.indexOf("a")); // возвращаемое значение 3
+console.log(x.indexOf("d")); // возвращаемое значение -1 (значение не найдено в массиве)
+console.log(x.indexOf("b", 3)); // возвращаемое значение 4
+console.log(x.indexOf(2, 2)); // возвращаемое значение -1 (значение не найдено в массиве)
+console.log(x.indexOf(1, -6)); // возвращаемое значение 0
+console.log(x.indexOf(1, -5)); // возвращаемое значение -1 (значение не найдено в массиве)
+/*
+В следующем примере мы рассмотрим с Вами как продолжить поиск искомого элемента
+в массиве после первого и последующих совпадений, вплоть до конца массива. Это позволит нам сравнить все элементы массива по определенному индексу на предмет наличия искомого элемента:
+*/
+
+var indexes = []; // инициализируем переменную, содержащую пустой массив
+var myArray = ['z', 'v', 'z', 'v', 'z', 'v']; // инициализируем переменную, содержащую массив строковых значений по которому будет произведен поиск
+var searchElement = 'z'; // инициализируем строковую переменную (значение переменной будем искать внутри массива myArray)
+var index = myArray.indexOf(searchElement); // инициализируем переменную, содержащую индекс первого искомого элемента (значение переменной searchElement) внутри массива myArray
+
+while (index != -1) { // пока значение переменной index не будет равно -1
+  indexes.push(index); // с использованием метода push() добавляем в переменную indexes значение переменной index
+  index = myArray.indexOf(searchElement, index + 1); // изменяем значение переменной путем поиска необходимого элемента далее в массиве (если найден - индекс элемента, если нет то -1)
+}
+
+console.log(indexes); // переменная содержит значение [0, 2, 4]
+
+/*
+Обратите внимание на то, что если вы ищете внутри массива NaN (Not a number - не число), независимо от наличия возвращаемое значение будет -1. Это поведение метода учтено в методе includes()ECMAScript 2016, добавленном в ECMAScript 2016, он решает подобную задачу, но возвращает логическое значение:
+*/
+
+var X = [NaN];  // инициализируем переменную, содержащую одно значение NaN
+
+console.log(X.indexOf(NaN)); // возвращаемое значение -1 (элемент не найден)
+console.log(X.includes(NaN)); // возвращаемое значение true (элемент найден)
+
+/////  lastIndexOf()  синтаксис array.lastIndexOf( searchElement, fromIndex )
+/////позволяет возвратить последний индекс искомого элемента в массиве при первом совпадении, или -1
+//lastIndexOf() использует оператор строгого сравнения (===) при сравнении искомого элемента и элементов массива.
+
+
+/*Пример использования */
+
+var xx = [1, 2, 3, "a", "b", "c"]; // инициализируем переменную, содержащую массив
+
+console.log(xx.lastIndexOf("a"));
+console.log(xx.lastIndexOf("d"));
+console.log(xx.lastIndexOf("b", 2));
+console.log(xx.lastIndexOf(1, -6));
+console.log(xx.lastIndexOf(1, -5));
+
+let frT = ["яблоки", "груши", "сливы", "яблоки", "груши"];
+
+console.log(frT.lastIndexOf("яблоки")); // 3
+
+var xxx = [1, 2, 3, "a", "b", "c"];
+console.log(xxx.lastIndexOf("b"));
+
+/*
+продолжить поиск искомого элемента с конца массива после первого и последующих совпадений, вплоть до начала массива. Это позволит нам сравнить все элементы массива по определенному индексу на предмет наличия искомого элемента:
+*/
+
+var Indexes = [];
+var MyArray = ['z', 'v', 'z', 'v', 'z', 'v', 'z', 'z'];
+
+var element = 'z';
+var Index = MyArray.lastIndexOf(element);
+
+while (Index != -1) {
+  Indexes.push(Index);
+  if (Index > 0) {
+    Index = MyArray.lastIndexOf(element, Index - 1);
+  } else {
+    Index = -1;
+  }
+}
+
+console.log(Indexes);
+/////////////////////////////////////////
+
+
+// every()
+/*
+Метод every() проверяет, все ли элементы соответствуют определенному условию:
+*/
+
+var numbers = [1, 8, 25, 42];
+var passed = numbers.every(condition);
+function condition(value, index, array) {
+  var result = false;
+  if (value > 0) {
+    result = true;
+  }
+  return result;
+}
+
+console.log(passed);
+//////////////////////////////
+
+// var numbers = [1, 8, 25, -42];
+
+// var passed = numbers.every(function (value, index, array) {
+//   var result = false;
+//   if (value > 0) {
+//     result = true;
+//   }
+//   return result;
+// });
+
+// console.log(numbers); 
+// console.log(passed); 
+
+
+// синтаксис:
+
+/*
+только с callback функцией
+array.every( function( currentValue, index, arr ) ); 
+*/
+
+/*
+с использованием объекта, на который может ссылаться ключевое слово this
+array.every( function( currentValue, index, arr ), thisValue ); 
+*/
+
+///////////////////////////////////////
+
+/*
+SOME()
+*/
+var NumbeRs = [1, -12, 8, -4, 25, 42];
+function Condition(value, index, array) {
+  var result = false;
+  if (value === 8) {
+    result = true;
+  }
+  return result;
+}
+var PasseD = NumbeRs.some(Condition); // true
+
+console.log(NumbeRs);
+console.log(PasseD);
+/////////////////
+
+let Nr = [1, 12, 4, 25, 42, "aaa", "fff", "pp"];
+let Pd = Nr.some(function (value, index, array) {
+  let result = false;
+  if (value < 0) {
+    result = true;
+  }
+  return result;
+});
+
+console.log(Nr);
+console.log(Pd);
+
+///////////////
+
+let NR = [-1, 12, 4, 25, 42, "aa"];
+function Condition(value, index, array) {
+  let result = false;
+  if (value === "aa") {
+    result = true;
+  }
+  return result;
+}
+let PD = NR.some(Condition); // true
+
+console.log(NR);
+console.log(PD);
+
+/*
+Метод filter(), как some() и every(), принимает функцию условия.
+ Но при этом возвращает массив тех элементов, 
+ которые соответствуют этому условию:
+*/
+
+var MYnumbers = [1, -12, 8, -4, 25, 42];
+function MYcondition(value, index, array) {
+  var result = false;
+  if (value > 0) {
+    result = true;
+  }
+  return result;
+}
+var filteredNumbers = MYnumbers.filter(MYcondition);
+
+for (var i = 0; i < filteredNumbers.length; i++) {
+  console.log(filteredNumbers[i]);
+}
+
+
+let Mynumbers = [1, -12, 8, -4, 123, 643, 97, 25, 42];
+let FilteredNumbers = Mynumbers.filter(function(value, index, array) {
+  let result = false;
+  if (value < 0) {
+    result = true;
+  }
+  return result;
+});
+
+for (var i = 0; i < FilteredNumbers.length; i++) {
+  console.log(FilteredNumbers[i]);
+}
+
+// REDUCE()
+
+let ArrNum = [1,2,3,4,5],
+
+res = ArrNum.reduce(function(sum, item) {
+  return sum + item;
+},0);
+
+console.log(res);
+
+let myNumArr = [[1,2,3], [7,8,9]];
+let newMyNum = myNumArr.reduceRight(function(sum, i) {
+  return sum.concat(i);
+},[]);
+
+console.log(newMyNum);
+
+// MAP()
+
+let Snum = [1, 2, 3, 4, 5, 6];
+function sq(val,i,arr) {
+  return  val * val;
+}
+
+let sqNum = Snum.map(sq);
+console.log(sqNum);
+
+let SnuM = [1, 2, 3, 4, 5, 6];
+let newsNum = SnuM.map(function(value, index, array) {
+  return value * 3;
+});
+console.log(newsNum);
+
+// аналогично с использованием стрелочной функции
+var array4 = [10, 20, 30];
+let double = array4.map( val => val * 2 );
+
+console.log( double ); 
+/* выводим значение переменной double равное [20, 40, 60] */
+
+let numbers6 = [2, 3, 4, 100]; 
+var MyObject = {
+  square: function( val ) {
+     return val * val; 
+  }
+};
+
+var squaRed = numbers6.map( 
+  function( val ) {
+    return this.square( val );
+     //  возвращаемое значение метода square объекта myObject
+  }, MyObject 
+);
+
+console.log(squaRed);
+
+//let li = document.querySelectorAll("li");
+//let li = document.getElementsByTagName("li");
+let li = document.getElementsByClassName("li");
+ 
+for (let i of li) {
+  i.style.backgroundColor = "blue";
+}
+
+// FOREACH
+
+let vN = [1, 2, 3];
+vN.forEach(fEh);
+function fEh(val, i, array) {
+ let res =  val * val;
+ console.log(res);
+}
+//////////////////////////////////////
+var NumBers = [2, 3, 4];
+var SquAred = []; 
+var myObjEct = { 
+  square: function( val ) { 
+    return val * val; 
+  }
+};
+
+NumBers.forEach(function( val ) {
+    SquAred.push( this.square( val ) ); 
+  }, myObjEct );
+
+console.log( SquAred );
+
+///////////////////////////
+
+var aRRay = [10, 20, 30]; 
+var SUm = 0; 
+
+aRRay.forEach( function (val) {
+    SUm += val;
+  });
+
+console.log( SUm ); // выводим значение переменной sum равное 60
