a34414280ed7971b1d930be3e44ab250a55ed164
ivanchikin <ivan.chikin.72@mail.ru>
2019-05-31 07:21:37 +0700

    19 урок подготовка

---
+++ Lessons_JS/19_Lesson/script.js
@@ -0,0 +1,23 @@
+let Box = document.getElementById("box"),
+	btn = document.getElementsByTagName("button"),
+	circle = document.getElementsByClassName("circle"),
+	heart = document.querySelectorAll(".heart"),
+	hearts = document.querySelectorAll(".wrap .heart"),
+	oneHeard = document.querySelector(".heart"),
+	oneDiv = document.querySelector("div"),
+	myInput = document.getElementsByName("myInput");
+
+	console.log(btn);
+	console.log(btn[1]);
+	console.log(circle);
+	console.log(circle[2]);
+	console.log(heart);
+	console.log(heart[2]);
+	console.log(hearts);
+	console.log(hearts[4]);
+	console.log(oneHeard);
+	console.log(oneDiv);
+	console.log(myInput);
+	console.log(myInput[2]);
+
+	
