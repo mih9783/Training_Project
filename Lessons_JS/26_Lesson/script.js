c2c3540ce30df707d2d76a1c2cc1e9f7d9d44ef3
ivanchikin <ivan.chikin.72@mail.ru>
2019-07-08 10:22:49 +0700

    26 урок продолжение

--- Lessons_JS/26_Lesson/script.js
+++ Lessons_JS/26_Lesson/script.js
@@ -2,7 +2,7 @@ function User(name, id) {
     this.name = name;
     this.id = id;
     this.human = true;
-    this.hello = function() {
+    this.hello = function () {
         console.log("Hello, " + this.name + "!");
     };
 }
@@ -15,3 +15,52 @@ console.log(alex);
 
 ivan.hello();
 alex.hello();
+
+//прототипы
+
+User.prototype.exit = function (name) {
+    console.log("Пользователь " + this.name + " ушёл со страницы сайта");
+};
+
+ivan.exit();
+
+
+
+class Persons {
+    constructor(name, id) {
+        this.name = name;
+        this.id = id;
+        this.human = true;
+    }
+
+    hey() {
+        console.log(`Hello! ${this.name}`);
+    }
+
+    exit() {
+        console.log(`Person ${this.name} покинул страницу сайта`);
+    }
+}
+
+const dima = new Persons("Dima", 42);
+const lena = new Persons("Lena", 32);
+
+dima.hey();
+lena.hey();
+dima.exit();
+lena.exit();
+
+
+class Person {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    sayName() {
+      //console.log(`Person ${this.name} said his name`);
+      console.log(`Person ${this.name} покинул страницу сайта`);
+    }
+  }
+  
+  const john = new Person("John");
+  john.sayName(); // Person John said his name
