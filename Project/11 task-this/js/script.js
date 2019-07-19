50be43d0e84d9d7f87f188805648c899ed4bea2e
ivanchikin <ivan.chikin.72@mail.ru>
2019-07-19 07:17:12 +0700

    11 задание закончил

--- Project/11 task-this/js/script.js
+++ Project/11 task-this/js/script.js
@@ -96,4 +96,25 @@ window.addEventListener("DOMContentLoaded", function() {
     }
 
     setClock("timer", deadline);
+
+    // модальное окно (modal)
+
+    let more = document.querySelector(".more"),
+        overlay = document.querySelector(".overlay"),
+        close = document.querySelector(".popup-close");
+
+        more.addEventListener("click", function() {
+            overlay.style.display = "block";
+            this.classList.add(".more-splash"); 
+            document.body.style.overflow = "hidden";// запрет прокрутки страницы   
+        });
+
+        close.addEventListener("click", function() {
+            overlay.style.display = "none";
+            more.classList.remove(".more-splash"); 
+            document.body.style.overflow = "";// запрет прокрутки страницы   
+        });
+
+
+        
 });
